a2b238cce58fdc9f7b0a19b310df7f0de17b619a
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-03 23:49:39 +0100

    getDeletePostroutes

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,28 +1,57 @@
-const fb = require('express').Router();
+const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
-// GET Route for retrieving all the feedback
-fb.get('/', (req, res) =>
-  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)))
-);
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  console.info('${req.method} request received for notes');
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
 
-// POST Route for submitting feedback
-fb.post('/', (req, res) => {
+//GET route for specific tip
+notes.get('/:note_id',(req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+  .then((data)=> JSON.parse(data))
+  .then((json)=> {
+    const result = json.filter((note)=> note.tip_id === tipID);
+    return result.length >0
+    ? res.json(result)
+    : res.json('No note with that ID');
+  })
+});
+
+//DELETE route for specific tip
+notes.delete('/:note_id',(req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+  .then((data)=> JSON.parse(data))
+  .then((json)=> {
+    //new array of allnotes except the one with UID
+    const result = json.filter((note)=> note.id !== noteID);
+    //save array to fs
+    writeToFile('./db/db.json', result);
+   //respond to DELTE request
+    res.json('Item ${noteID} has been deleted.');
+  })
+});
+
+// POST Route for new note
+notes.post('/', (req, res) => {
   // Destructuring assignment for the items in req.body
-  const { email, feedbackType, feedback } = req.body;
+  console.log(req.body);
+  const { title,text } = req.body;
 
   // If all the required properties are present
-  if (email && feedbackType && feedback) {
+  if (req.body) {
     // Variable for the object we will save
-    const newFeedback = {
-      email,
-      feedbackType,
-      feedback,
-      feedback_id: uuidv4(),
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
     };
 
-    readAndAppend(newFeedback, './db/feedback.json');
+    readAndAppend(newNote, './db/feedback.json');
 
     const response = {
       status: 'success',
